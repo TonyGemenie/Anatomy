2fecd46add672d20dad0548778ab167b89c51989
Tony Gemenie <tony.gemenie@gmail.com>
2018-06-11 20:21:13 -0500

    Refactor

--- app/src/main/java/prototype/anatomy/anatomy/MainActivity.java
+++ app/src/main/java/prototype/anatomy/anatomy/MainActivity.java
@@ -1,93 +1,91 @@
 package prototype.anatomy.anatomy;
 
 import android.animation.ObjectAnimator;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public String[] structures;
     @BindView(R.id.start_button) Button startBtn;
-    @BindView(R.id.answer_button_1) Button answerBtn1;
-    @BindView(R.id.answer_button_2) Button answerBtn2;
-    @BindView(R.id.answer_button_3) Button answerBtn3;
-    @BindView(R.id.answer_button_4) Button answerBtn4;
-    @BindView(R.id.answer_text_1) TextView answerText1;
-    @BindView(R.id.answer_text_2) TextView answerText2;
-    @BindView(R.id.answer_text_3) TextView answerText3;
-    @BindView(R.id.answer_text_4) TextView answerText4;
-    @BindView(R.id.answer_text_5) TextView answerText5;
-    @BindView(R.id.answer_text_6) TextView answerText6;
-    @BindView(R.id.answer_text_7) TextView answerText7;
-    @BindView(R.id.answer_text_8) TextView answerText8;
-    @BindView(R.id.answer_text_9) TextView answerText9;
-    @BindView(R.id.answer_text_10) TextView answerText10;
-    @BindView(R.id.answer_text_11) TextView answerText11;
-    @BindView(R.id.answer_text_12) TextView answerText12;
-    public Button[] buttons = new Button[]{answerBtn1, answerBtn2, answerBtn3, answerBtn4};
-    public TextView[] textViews = new TextView[]{answerText1, answerText2, answerText3, answerText4, answerText5, answerText6, answerText7,
-            answerText8, answerText9, answerText10, answerText11, answerText12};
-    public boolean[] answered = new boolean[textViews.length];
+    @BindViews({R.id.answer_text_1, R.id.answer_text_2, R.id.answer_text_3, R.id.answer_text_4, R.id.answer_text_5, R.id.answer_text_6, R.id.answer_text_7,
+            R.id.answer_text_8, R.id.answer_text_9, R.id.answer_text_10, R.id.answer_text_11, R.id.answer_text_12}) TextView[] answerTexts;
+    @BindViews({R.id.answer_button_1, R.id.answer_button_2, R.id.answer_button_3, R.id.answer_button_4}) Button[] buttons;
+    public boolean[] answered;
     @BindView(R.id.result_text)TextView result;
     @BindView(R.id.skin_image) ImageView image;
     Random mRnd;
     int aI;
     int correct;
+    int colorValue;
+
+    static final ButterKnife.Setter<TextView, Integer> BLANKS = new ButterKnife.Setter<TextView, Integer>() {
+        @Override
+        public void set(@NonNull TextView view, Integer value, int index) {
+            view.setTextColor(value);
+        }
+    };
+
+    static final ButterKnife.Setter<TextView, String> STRINGS = new ButterKnife.Setter<TextView, String>() {
+        @Override
+        public void set(@NonNull TextView view, String question, int index) {
+            view.setText(question);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        colorValue = getResources().getColor(R.color.colorWhite);
         mRnd = new Random();
         structures = getResources().getStringArray(R.array.structures_array);
     }
 
     public void askQuestion() {
         startBtn.setVisibility(View.GONE);
-
         aI = mRnd.nextInt(structures.length);
-        int[] iS = new int[3];
-        int incorrectAnswerIndex = 0;
+        while(answered[aI]) {
+            aI = mRnd.nextInt(structures.length);
+        }
+        int[] iS = new int[]{1000, 2000, 3000, 4000};
         for (int i = 0; i < iS.length; i++) {
-            while (iS[0] == iS[1] || iS[1] == iS[2] || iS[0] == iS[2] ||
-                    iS[0] == aI || iS[1] == aI || iS[2] == aI) {
-                incorrectAnswerIndex = mRnd.nextInt(structures.length);
-            }
-            iS[i] = incorrectAnswerIndex;
+            do{
+                int incorrectAnswerIndex = mRnd.nextInt(structures.length);
+                iS[i] = incorrectAnswerIndex;
+                }while (iS[0] == iS[1] || iS[1] == iS[2] || iS[0] == iS[2] || iS[0] == iS[3] || iS[1] == iS [3] || iS[2] == iS[3] ||
+                    iS[0] == aI || iS[1] == aI || iS[2] == aI || iS[3] == aI);
         }
         correct = mRnd.nextInt(buttons.length);
+        iS[correct] = aI;
         for (int i = 0; i < buttons.length; i++) {
-            if(i == correct){
-                buttons[i].setText(structures[aI]);
-            }else {
-                buttons[i].setText(structures[iS[i]]);
-            }
+            buttons[i].setText(structures[iS[i]]);
         }
-
         int colorValue = getResources().getColor(R.color.colorAccent);
-        textViews[aI].setTextColor(colorValue);
+        answerTexts[aI].setTextColor(colorValue);
     }
 
     public void answerQuestion (View view) {
         result.setText("");
         result.setAlpha(1f);
-        if (buttons[correct] == view) {
-            textViews[aI].setText(structures[aI]);
-            int colorValue = getResources().getColor(R.color.colorWhite);
-            textViews[aI].setTextColor(colorValue);
-            answered[aI] = true;
+        if (String.valueOf(correct).equals(view.getTag().toString())) {
+            answerTexts[aI].setText(structures[aI]);
+            answerTexts[aI].setTextColor(colorValue);
             result.setText(getString(R.string.right));
+            answered[aI] = true;
             if (checkStructures()){
                 askQuestion();
             } else {
@@ -105,22 +103,19 @@ public class MainActivity extends AppCompatActivity {
     public void playAgain(View view) {
         startBtn.setVisibility(View.GONE);
         setBlanks();
-        answered = new boolean[textViews.length];
-
+        answered = new boolean[structures.length];
         askQuestion();
     }
 
     public void setBlanks(){
-        for (int i = 0; i < textViews.length; i++) {
-            int colorValue = getResources().getColor(R.color.colorWhite);
-            textViews[i].setTextColor(colorValue);
-            textViews[i].setText(getString(R.string.question));
-        }
+        String question = getString(R.string.question);
+        ButterKnife.apply(answerTexts, BLANKS, colorValue);
+        ButterKnife.apply(answerTexts, STRINGS, question);
     }
 
     public boolean checkStructures(){
         for(boolean answer: answered){
-            if(answer){
+            if(!answer){
                 return true;
             }
         }
